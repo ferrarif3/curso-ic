1b585df046583867868c57c4c068cfadc531fa5e
FRANCO FERRARI <fferrari@ar0fvfxj14lhv2j.ml.com>
2018-11-29 11:04:17 -0300

    Cambios en application-ic y agrega sql server a deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker stop curso
 docker rm cursodocker
-docker run --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+docker run --link sql_server:sql_server --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
