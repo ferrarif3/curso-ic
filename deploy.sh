5ddef30a636a9efc27c89bf9fdf427bf7076604e
FRANCO FERRARI <fferrari@ar0fvfxj14lhv2j.ml.com>
2018-11-29 13:35:47 -0300

    prueba de funcionamiento en jenkins

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker stop curso
 docker rm curso
-docker run --link sql_server:sql-server --link some-redis1:some-redis --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+docker run --link sql-server:sql-server --link some-redis:some-redis --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
